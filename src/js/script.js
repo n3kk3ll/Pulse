5520bf7f0ac387cca8c18b8e5ec3847163fd2801
Andrii <example.gmail.com>
2021-11-02 01:23:56 +0300

    added slider, made it resposive

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,35 @@
-$(document).ready(function () {
-    $('.carousel__wrapper').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false,
-                }
-            },
-        ]
-    });
+// $(document).ready(function () {
+//     $('.carousel__wrapper').slick({
+//         speed: 1200,
+//         // adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false,
+//                 }
+//             },
+//         ]
+//     });
+// });
+
+const slider = tns({
+    container: '.carousel__wrapper',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    speed: 1000,
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
 });
