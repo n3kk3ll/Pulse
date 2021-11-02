06d091fdd4dbba7d45ab9797369e61700abe635d
Andrii <example.gmail.com>
2021-11-03 01:48:01 +0200

    made form validation and phone input mask

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,17 @@
 // Modal
 $(function () {
-    $('[data-modal="consultation"').on('click', function () {
+    $('[data-modal=consultation]').on('click', function () {
         $('.overlay, #consultation').fadeIn('fast');
     });
-    $('.modal__close, .overlay').on('click', function () {
-        $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
     });
-    $('.button_mini').each(function (i) {
-        $(this).on('click', function () {
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-            $('.overlay, #order').fadeIn('fast');
+            $('.overlay, #order').fadeIn('slow');
         });
     });
 });
